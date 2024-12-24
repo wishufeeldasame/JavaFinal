de5972a10145a52cf504893004bdf3236306c182
wishufeeldasame <bubblegrape@kakao.com>
2024-12-24 13:22:39 +0900

    창생성

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,5 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
-import java.io.*;
-import java.util.*;
-
 public class Main {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            new ManagementApp();
-        });
+        new ManagementApp();
     }
 }
--- src/ManagementApp.java
+++ src/ManagementApp.java
@@ -1,3 +1,22 @@
-public class ManagementApp {
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.BorderLayout;
+import java.io.*;
+import java.util.*;
 
+public class ManagementApp extends JFrame {
+    private List<Lecture> lectures = new ArrayList<>();
+    private List<Student> students = new ArrayList<>();
+    private List<Grade> grades = new ArrayList<>();
+    private final String FILE_NAME = "data.txt";
+
+    public ManagementApp() {
+        setTitle("성적 관리 프로그램");
+        setSize(800, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+
+
+        setVisible(true);
+    }
 }
