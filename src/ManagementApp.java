501efc85158b28e04d81f432bc1f79ac0559f967
wishufeeldasame <bubblegrape@kakao.com>
2024-12-24 13:27:12 +0900

    ad

--- src/ManagementApp.java
+++ src/ManagementApp.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import javax.swing.border.Border;
+
 import java.awt.event.*;
 import java.awt.BorderLayout;
 import java.io.*;
@@ -16,7 +18,45 @@ public class ManagementApp extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
 
+        JTabbedPane tabbedPane = new JTabbedPane();
+        tabbedPane.addTab("강의 관리", new LecturePanel());
+        tabbedPane.addTab("학생 관리", new StudentPanel());
+        tabbedPane.addTab("성적 등록", new GradePanel());
+        tabbedPane.addTab("성적 조회", new GradeQueryPanel());
+
+        add(tabbedPane, BorderLayout.CENTER);
+
+        try{
+            loadData();
+        } catch (IOException e){
+            JOptionPane.showMessageDialog(this, "파일 로드 중 오류 발생: " + e.getMessage(), "오류", JOptionPane.ERROR_MESSAGE);
+        }
 
         setVisible(true);
     }
+
+    private void saveData() throws IOException{
+
+    }
+
+    private void loadData() throws IOException{
+
+    }
+
+    class LecturePanel extends JPanel{
+
+    }
+
+    class StudentPanel extends JPanel{
+
+    }
+
+    class GradePanel extends JPanel{
+
+    }
+
+    class GradeQueryPanel extends JPanel{
+
+    }
+
 }
